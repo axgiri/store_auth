6fb3905323db9580969a621d8246744325bca1c8
axgiri <reiaogiri@gmail.com>
2026-01-19 17:07:43 +0100

    init

---
+++ src/test/java/com/github/oldlab_auth/OldlabAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oldlab_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldlabAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
